07285ed244b11ecfcb8ef4200c42ef5b448cbdf3
Frederic Marion-Poll <marion_poll@hotmail.com>
2018-08-06 18:23:18 +0200

    add file not taken into account so far

---
+++ src/plugins/fmp/areatrack/MeasureAndName.java
@@ -0,0 +1,14 @@
+package plugins.fmp.areatrack;
+
+public class MeasureAndName {
+	public String name;
+	public double data;
+	public double count;
+
+	public MeasureAndName(String name, double data, double count) {
+		this.name = name;
+		this.data = data;
+		this.count = count;
+	}
+
+}
