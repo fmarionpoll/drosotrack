7cb6f470a86d98812dc249b99c0310dafc223e08
Frederic Marion-Poll <marion_poll@hotmail.com>
2019-07-03 08:10:51 +0200

    changes back and forth to a renderer to introduce an index of the experiment selected

--- src/plugins/fmp/tools/ComboBoxWithIndexTextRenderer.java
+++ src/plugins/fmp/tools/ComboBoxWithIndexTextRenderer.java
@@ -17,8 +17,9 @@ public class ComboBoxWithIndexTextRenderer  extends DefaultListCellRenderer {
       	Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
       	ListModel<Class<?>> model = (ListModel<Class<?>>) list.getModel();
       	int nitems = model.getSize();
-      	if (index < 0)
+      	if (index < 0) {
       		index = list.getSelectedIndex();
+      		}   
 		String lead = "["+(index+1)+":"+nitems+"] ";
 		setText(lead+(String) value);
 //		c.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
